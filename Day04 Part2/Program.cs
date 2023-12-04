54c566701ac85f6a0a65f2977ebdfe7de7583533
Charles Jordan <zerovax@gmail.com>
2023-12-04 22:07:50 +0000

    Completed Day04 Part2

--- Day04 Part2/Program.cs
+++ Day04 Part2/Program.cs
@@ -1,4 +1,6 @@
-﻿string inputFile;
+﻿using System.Net.Http.Headers;
+
+string inputFile;
 
 #if DEBUG
 inputFile = "text.txt";
@@ -8,7 +10,65 @@ inputFile = "input.txt";
 
 int result = 0;
 string[] input = File.ReadAllLines(inputFile);
+Dictionary<int, ScratchCard> ScratchCards = new();
+
+/* add all scratch cards*/
+for (int i = 0; i < input.Length; i++)
+{
+    ScratchCard card = new(input[i].Substring(input[i].IndexOf(':') + 1).Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+    ScratchCards.Add(i, card);
+}
 
 
-Console.WriteLine(result);
+foreach (KeyValuePair<int,ScratchCard> card in ScratchCards)
+{
+    int MatchingNumbers = card.Value.MatchingNumbers();
+    if( MatchingNumbers > 0 )
+    {
+        IEnumerable<int> cardsWon = Enumerable.Range(card.Key + 1, MatchingNumbers);
+        foreach(int i in cardsWon)
+        {
+            ScratchCards[i].CardCount += card.Value.CardCount;
+        }
+    }
+    result += card.Value.CardCount;
+}
+
+
+
+
+Console.WriteLine($"Result: {result}");
 Console.ReadLine();
+
+
+class ScratchCard
+{
+    private int[] _validNumbers;
+    private int[] _chosenNumbers;
+    public int CardCount;
+
+    public ScratchCard(string[] cardText)
+    {
+        CardCount = 1;
+        string[] vNumbers = cardText[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        _validNumbers = Array.ConvertAll(vNumbers, s => int.Parse(s));
+        string[] cNumbers = cardText[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        _chosenNumbers = Array.ConvertAll(cNumbers, s => int.Parse(s));
+    }
+
+    public void PrintVaildNumbers()
+    {
+        Console.WriteLine("Valid Numbers:");
+        foreach (int i in _validNumbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public int MatchingNumbers()
+    {
+        return _chosenNumbers
+            .Where(c => _validNumbers.Contains(c))
+            .Count();
+    }
+}
