5d9675888eb94d128804800ed658cae51f4104b1
Charles Jordan <zerovax@gmail.com>
2023-12-04 21:15:21 +0000

    Solved Day04 Part1

--- Day04 Part1/Program.cs
+++ Day04 Part1/Program.cs
@@ -1,4 +1,6 @@
-﻿string inputFile;
+﻿using System.Net.Http.Headers;
+
+string inputFile;
 
 #if DEBUG
 inputFile = "text.txt";
@@ -8,13 +10,51 @@ inputFile = "input.txt";
 
 int result = 0;
 string[] input = File.ReadAllLines(inputFile);
+
+
 foreach (string line in input)
 {
-    Console.WriteLine(line);
+    ScratchCard card = new(line.Substring(line.IndexOf(':') + 1).Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+    result += card.PointsTotal();
+    
 }
 
 
-Console.WriteLine(result);
+Console.WriteLine($"Result: {result}");
 Console.ReadLine();
 
 
+class ScratchCard
+{
+    private int[] _validNumbers;
+    private int[] _chosenNumbers;
+
+    public ScratchCard(string[] cardText)
+    {
+        string[] vNumbers = cardText[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        _validNumbers = Array.ConvertAll(vNumbers, s => int.Parse(s));
+        string[] cNumbers = cardText[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        _chosenNumbers = Array.ConvertAll(cNumbers, s => int.Parse(s));
+    }
+
+    public void PrintVaildNumbers()
+    {
+        Console.WriteLine("Valid Numbers:");
+        foreach(int i in _validNumbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public int PointsTotal()
+    {
+        return (int)Math.Pow(2, MatchingNumbers() - 1);
+    }
+
+    private int MatchingNumbers()
+    {
+        return _chosenNumbers
+            .Where(c => _validNumbers.Contains(c))
+            .Count();
+    }
+}
