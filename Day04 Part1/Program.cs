0a953e3b7ca61d4c6dea5842f95f3e6367e6d1f8
Charles Jordan <zerovax@gmail.com>
2023-12-04 20:39:30 +0000

    Project Setup

--- Day04 Part1/Program.cs
+++ Day04 Part1/Program.cs
@@ -3,12 +3,18 @@
 #if DEBUG
 inputFile = "text.txt";
 #else
-    inputFile = "input.txt";
+inputFile = "input.txt";
 #endif
 
 int result = 0;
 string[] input = File.ReadAllLines(inputFile);
+foreach (string line in input)
+{
+    Console.WriteLine(line);
+}
 
 
 Console.WriteLine(result);
 Console.ReadLine();
+
+
