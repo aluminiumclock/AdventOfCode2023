a3aeca367f1bb2c25e0ff92eebf2ac3005d0099b
Charles Jordan <Charles.Jordan@turners-distribution.com>
2023-12-07 09:27:31 +0000

    Revert to previous version

--- Day03 Part2/Program.cs
+++ Day03 Part2/Program.cs
@@ -1,16 +1,14 @@
 ﻿string inputFile;
 
-
+#if DEBUG
+    inputFile = "test.txt";
+#else
     inputFile = "input.txt";
-
+#endif
 
 int result = 0;
 
 string[] lines = File.ReadAllLines(inputFile);
-Dictionary<int, List<NumberInString>> numbersInText = new();
-
-LoadNumbers(lines, numbersInText);
-
 for (int i = 0; i < lines.Length; i++)
 {
     for (int j = 0; j < lines[i].Length; j++)
@@ -22,20 +20,20 @@ for (int i = 0; i < lines.Length; i++)
 
             if (i > 0)
             {
-                StringNumberData lt = NearbyNumbers(i - 1, j);
-                fullNumber *= lt.Fullnumber;
+                LineTotal lt = NearbyNumbers(lines[i - 1], j);
+                fullNumber *= lt.totalOnLine;
                 numbersByGear += lt.numbersAdjGearOnLine;
             }
 
-            StringNumberData clt = NearbyNumbers(i, j);
-            fullNumber *= clt.Fullnumber;
+            LineTotal clt = NearbyNumbers(lines[i], j);
+            fullNumber *= clt.totalOnLine;
             numbersByGear += clt.numbersAdjGearOnLine;
 
             if (i + 1 < lines.Length)
             {
 
-                StringNumberData lt = NearbyNumbers(i + 1, j);
-                fullNumber *= lt.Fullnumber;
+                LineTotal lt = NearbyNumbers(lines[i + 1], j);
+                fullNumber *= lt.totalOnLine;
                 numbersByGear += lt.numbersAdjGearOnLine;
             }
 
@@ -51,66 +49,84 @@ for (int i = 0; i < lines.Length; i++)
 Console.WriteLine(result);
 Console.ReadLine();
 
-StringNumberData NearbyNumbers (int line, int position)
+LineTotal NearbyNumbers (string v, int position)
 {
-    List<NumberInString> numbers = numbersInText[line];
+    int total = 1;
+    int numbersOnLine = 0;
+    int index = position == 0 ? 0 : position - 1;
 
-    return new StringNumberData
+    if (Char.IsDigit(v[index]))
     {
-        total = numbers.Where(n => n.Touching(position)).Aggregate<NumberInString, int>(1, (p, n) => p * n.Number),
-        numbersAdjGearOnLine = numbers.Where(n => n.Touching(position)).Count()
+        int start = index;
+        while (start > 0 && Char.IsDigit(v[start - 1]))
+        {
+            start--;
+        }
+        int end = index;
+        while (end + 1 < v.Length && Char.IsDigit(v[end + 1]))
+        {
+            end++;
+        }
+        total *= int.Parse(v.Substring(start, end - start + 1));
+        numbersOnLine++;
+        index = end;
+    }
+
+    if(index >= position)
+    {
+        return new LineTotal
+        {
+            totalOnLine = total,
+            numbersAdjGearOnLine = numbersOnLine
+        };
+    }
+    index = position;
+
+    if (Char.IsDigit(v[index]))
+    {
+        int start = index;
+        int end = index;
+        while (end + 1 < v.Length && Char.IsDigit(v[end + 1]))
+        {
+            end++;
+        }
+        total *= int.Parse(v.Substring(start, end - start + 1));
+        numbersOnLine++;
+
+        index = end;
+    }
+
+    if (index >= position+1)
+    {
+        return new LineTotal
+        {
+            totalOnLine = total,
+            numbersAdjGearOnLine = numbersOnLine
+        };
+    }
+    index = position + 1;
+
+    if (Char.IsDigit(v[index]))
+    {
+        int start = index;
+        int end = index;
+        while (end + 1 < v.Length && Char.IsDigit(v[end + 1]))
+        {
+            end++;
+        }
+        total *= int.Parse(v.Substring(start, end - start + 1));
+        numbersOnLine++;
+    }
+
+    return new LineTotal
+    {
+        totalOnLine = total,
+        numbersAdjGearOnLine = numbersOnLine
     };
 }
 
-static void LoadNumbers(string[] lines, Dictionary<int, List<NumberInString>> numbersInText)
+struct LineTotal
 {
-    for (int x = 0; x < lines.Length; x++)
-    {
-        string v = lines[x];
-        List<NumberInString> numbers = new();
-        for (int i = 0; i < v.Length; i++)
-        {
-            if (Char.IsDigit(v[i]))
-            {
-                int start = i;
-                while (i + 1 < v.Length && Char.IsDigit(v[i + 1]))
-                {
-                    i++;
-                }
-                NumberInString numberInString = new NumberInString
-                {
-                    Number = Int32.Parse(v.Substring(start, i - start + 1)),
-                    Start = start,
-                    End = i
-                };
-                numbers.Add(numberInString);
-            }
-        }
-        numbersInText.Add(x, numbers);
-    }
-}
-
-struct NumberInString
-{
-    public int Number;
-    public int Start;
-    public int End;
-
-    public bool Touching(int position)
-    {
-        return Start - 1 <= position
-                && position <= End + 1;
-    }
-
-    public override string ToString()
-    {
-        return $"Number {Number} starts at {Start} and ends at {End}";
-    }
-}
-
-struct StringNumberData
-{
-    public int total;
+    public int totalOnLine;
     public int numbersAdjGearOnLine;
-    public int Fullnumber => total == 0 ? 1 : total;
 }
