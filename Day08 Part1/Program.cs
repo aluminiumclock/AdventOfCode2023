444da4a26e15899ddac0b13b9fffab7e0e70032a
aluminiumClock <zerovax@gmail.com>
2023-12-08 11:47:42 +0000

    Completed Day08 Part1

--- Day08 Part1/Program.cs
+++ Day08 Part1/Program.cs
@@ -1,4 +1,7 @@
-﻿string inputFile;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+string inputFile;
 
 #if DEBUG
     inputFile = "test.txt";
@@ -6,12 +9,40 @@
     inputFile = "input.txt";
 #endif
 
-int result = 0;
 string[] input = File.ReadAllLines(inputFile);
-foreach (string line in input)
+var instructions = input[0];
+Dictionary<string, Node> nodes = new();
+
+foreach (string line in input.Skip(2))
 {
-    Console.WriteLine(line);
+    var lineSplit = line.Split(new char[] { '=',','}, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    Node newNode = new Node(lineSplit[1][1..], lineSplit[2][..3]);
+    nodes.Add(lineSplit[0][..3], newNode);
 }
 
-Console.WriteLine(result);
+string nextNode = "AAA";
+int steps = 0;
+
+while (nextNode != "ZZZ")
+{
+    char instruction = instructions[steps % instructions.Length];
+    nextNode = instruction == 'R' ? nodes[nextNode].Right : nodes[nextNode].Left;
+    steps++;
+}
+
+
+
+Console.WriteLine(steps);
 Console.ReadLine();
+
+class Node
+{
+    public Node(string left, string right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public string Left { get; set; }
+    public string Right { get; set; }
+}
