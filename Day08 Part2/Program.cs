48f838dd41ac9392acc555cdafedc1ffe53c4e28
aluminiumClock <zerovax@gmail.com>
2023-12-08 15:09:06 +0000

    unfinished (swapping PCs)

--- Day08 Part2/Program.cs
+++ Day08 Part2/Program.cs
@@ -1,17 +1,62 @@
-﻿string inputFile;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Linq;
+
+string inputFile;
 
 #if DEBUG
     inputFile = "test.txt";
 #else
-    inputFile = "input.txt";
+inputFile = "input.txt";
 #endif
 
-int result = 0;
 string[] input = File.ReadAllLines(inputFile);
-foreach (string line in input)
+var instructions = input[0];
+Dictionary<string, Node> nodes = new();
+
+foreach (string line in input.Skip(2))
 {
-    Console.WriteLine(line);
+    var lineSplit = line.Split(new char[] { '=', ',' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    Node newNode = new Node(lineSplit[1][1..], lineSplit[2][..3]);
+    nodes.Add(lineSplit[0][..3], newNode);
 }
 
-Console.WriteLine(result);
+
+
+var ANodes = nodes.Where(x => x.Key[2] == 'A').Select(x => x.Key);
+bool AllZNodes = false;
+int steps = 0;
+
+while (!AllZNodes)
+{
+    char instruction = instructions[steps % instructions.Length];
+    ANodes = ANodes.Select(x => nodes[x].Next(instruction));
+    AllZNodes = !ANodes.Where(x => x[2] != 'Z').Any();
+    steps++;
+    if(steps % 10000 == 0)
+    {
+        Console.WriteLine(steps);
+    }
+}
+
+
+
+Console.WriteLine(steps);
 Console.ReadLine();
+
+class Node
+{
+    public Node(string left, string right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public string Left { get; set; }
+    public string Right { get; set; }
+
+    public string Next(char instruction)
+    {
+        return instruction == 'R' ? Right : Left;
+    }
+}
