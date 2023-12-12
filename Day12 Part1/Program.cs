8ae488558b231841a4e9694f0d1a71b2379cfdb3
Charles Jordan <zerovax@gmail.com>
2023-12-12 22:22:38 +0000

    stopgap

--- Day12 Part1/Program.cs
+++ Day12 Part1/Program.cs
@@ -1,4 +1,8 @@
-﻿string inputFile;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+string inputFile;
 
 #if DEBUG
     inputFile = "test.txt";
@@ -6,12 +10,67 @@
     inputFile = "input.txt";
 #endif
 
+List<SpringRow> rows = File.ReadAllLines(inputFile).Select(x => x.Split(' ')).Select(y => new SpringRow(y[0], y[1])).Take(1).ToList();
+
 int result = 0;
-string[] input = File.ReadAllLines(inputFile);
-foreach (string line in input)
+Dictionary<(string, int[]), int> PermutationCache = new();
+int counter = 0;
+
+foreach(var springRow in rows)
 {
-    Console.WriteLine(line);
+    result += Permutations(springRow.line, springRow.brokenSpring);
 }
 
+Console.WriteLine(counter);
 Console.WriteLine(result);
 Console.ReadLine();
+
+int Permutations(string line, int[] brokenSprings)
+{
+    //check if we've already done this lookup
+    if(PermutationCache.TryGetValue((line, brokenSprings), out var value))
+    {
+        return value;
+    }
+
+    if(brokenSprings.Length == 0)
+    {
+        return line.All(x => x != '#') ? 1 : 0;
+    }
+    if (line.Length < brokenSprings.Sum())
+    {
+        return 0;
+    }
+    if (line[0] == '.')
+    {
+        return Permutations(line[1..], brokenSprings);
+    } 
+    //choose to go with current number or skip
+    int current = (line.Substring(0, brokenSprings[0]).All(x => x != '.') 
+                        && line.Length > brokenSprings[0] 
+                        && line.Substring(brokenSprings[0])[0] != '#'
+                    || line.Length <= brokenSprings[0]) ?
+        Permutations(line[brokenSprings[0]..], brokenSprings[1..])
+        : 0;
+
+
+    int skipped = line[0] == '?' ? Permutations(line[1..], brokenSprings) : 0;
+
+    int result = current + skipped;
+    PermutationCache.Add((line, brokenSprings), result);
+
+    counter++;
+    return result;
+}
+
+class SpringRow
+{
+    public string line;
+    public int[] brokenSpring;
+
+    public SpringRow(string inputLine, string brokenSprings)
+    {
+        line = inputLine;
+        brokenSpring = brokenSprings.Split(',').Select(x => int.Parse(x)).ToArray();
+    }
+}
